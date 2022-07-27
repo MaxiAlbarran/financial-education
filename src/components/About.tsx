6af6afbcaa9f2ba9b86a97e291121400b1d1feb3
Maximiliano Albarran <maxialbarran@hotmail.com>
2022-07-26 21:08:12 -0300

    About component created

---
+++ src/components/About.tsx
@@ -0,0 +1,28 @@
+import '../styles/About.css'
+
+import john from '../images/john-doe.png'
+
+const About = () => {
+  return (
+    <div className="about">
+
+      <div className="container">
+        <img src={john} alt="Profile picture" />
+
+        <div className="col-2">
+          <h2>About</h2>
+          <span className="line"></span>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aperiam ipsa minus voluptate ratione illum debitis, alias, asperiores libero, sunt commodi nulla facere hic aliquam?</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores ipsa modi excepturi, odit maxime laborum!</p>
+
+          <button className="button">Explore more</button>
+
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default About
